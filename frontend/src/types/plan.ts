6ad67c96556146a9e309bd4729d26435041c450f
jlli35 <jlli35@iflytek.com>
2025-04-25 10:52:23 +0800

    提价测试

--- frontend/src/types/plan.ts
+++ frontend/src/types/plan.ts
@@ -1,9 +1,8 @@
+import planData from "@/types/plan.yaml";
 import {
   PlanType,
   planTypeFromJSON,
 } from "@/types/proto/v1/subscription_service";
-// import planData from "./plan.yaml";
-import planData from "../types/plan.yaml";
 
 // Check api/plan.go to understand what each feature means.
 export type FeatureType =
