66d4a569b03d656ccc0ca7042727fa7cf6600349
jlli35 <jlli35@iflytek.com>
2025-04-27 15:22:55 +0800

    还原代码

--- frontend/src/types/iam/index.ts
+++ frontend/src/types/iam/index.ts
@@ -1,10 +1,5 @@
-import { readFileSync } from "fs";
-import { load } from "js-yaml";
-import { join } from "path";
 import type { Permission } from "./permission";
-
-const permissionYaml = readFileSync(join(__dirname, "permission.yaml"), "utf8");
-const PERMISSION_DATA = load(permissionYaml) as { permissions: Permission[] };
+import PERMISSION_DATA from "./permission.yaml";
 
 export const PERMISSIONS: Permission[] = PERMISSION_DATA.permissions;
 
